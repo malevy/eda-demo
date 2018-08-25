d9740ea3922df194ed2169db962e5a23b5c37092
Michael Levy <malevy@outlook.com>
2018-08-25 16:23:58 -0400

    order service resolves other services using eureka

--- eda-order-service/src/main/java/net/malevy/edaorder/config/ExternalServicesProperties.java
+++ eda-order-service/src/main/java/net/malevy/edaorder/config/ExternalServicesProperties.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -21,21 +22,17 @@ public class ExternalServicesProperties {
     @Autowired
     public ExternalServicesProperties(
             @Value("${app.services.reservation.host}") String reservationHost,
-            @Value("${app.services.reservation.port}") String reservationPort,
-            @Value("${app.services.payment.host}") String paymentHost,
-            @Value("${app.services.payment.port}") String paymentPort) {
+            @Value("${app.services.payment.host}") String paymentHost) {
 
         payment = UriComponentsBuilder.newInstance()
                 .scheme("http")
                 .host(paymentHost)
-                .port(paymentPort)
                 .build().toUri();
         log.info("payment-service: {}", payment.toString());
 
         reservation = UriComponentsBuilder.newInstance()
                 .scheme("http")
                 .host(reservationHost)
-                .port(reservationPort)
                 .build().toUri();
         log.info("reservation-service: {}", reservation.toString());
     }
